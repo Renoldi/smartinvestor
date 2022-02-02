ef956f9ed591bd4d835bb4792ac71de94be43738
renoldiboz <renoldiboz@gmail.com>
2022-02-02 12:41:32 +0700

    change

--- app/Views/include/header.php
+++ app/Views/include/header.php
@@ -16,14 +16,14 @@
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- endor CSS Files -->
-  <link href="<?= base_url() . '/assets/endor/aos/aos.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/bootstrap/css/bootstrap.min.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/bootstrap-icons/bootstrap-icons.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/boxicons/css/boxicons.min.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/glightbox/css/glightbox.min.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/remixicon/remixicon.css' ?>" rel="stylesheet">
-  <link href="<?= base_url() . '/assets/endor/swiper/swiper-bundle.min.css' ?>" rel="stylesheet">
+  <!-- vendor CSS Files -->
+  <link href="<?= base_url() . '/assets/vendor/aos/aos.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/bootstrap/css/bootstrap.min.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/boxicons/css/boxicons.min.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/glightbox/css/glightbox.min.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/remixicon/remixicon.css' ?>" rel="stylesheet">
+  <link href="<?= base_url() . '/assets/vendor/swiper/swiper-bundle.min.css' ?>" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url() . '/assets/css/style.css' ?>" rel="stylesheet">
