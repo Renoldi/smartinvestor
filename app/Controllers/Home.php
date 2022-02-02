7bdcb072c8127f1c16bdabada03f0fbd7279c8eb
renoldiboz <renoldiboz@gmail.com>
2022-02-02 18:21:58 +0700

    change

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,7 +17,7 @@ class Home extends BaseController
     {
 
         $faqs = new Faq();
-        $paginate =  $faqs->paginate(2,'fags');
+        $paginate =  $faqs->paginate(1,'fags');
         $pager =  $faqs->pager;
         $data = [
             'title' => ucfirst("smartinvestor"),
