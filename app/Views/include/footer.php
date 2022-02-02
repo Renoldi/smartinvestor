3b7db2d52cbade728555b5734603c60bb0d5937e
renoldiboz <renoldiboz@gmail.com>
2022-02-02 12:05:16 +0700

    change

--- app/Views/include/footer.php
+++ app/Views/include/footer.php
@@ -76,13 +76,13 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="<?= base_url().'/assets/vendor/purecounter/purecounter.js'?>"></script>
-  <script src="<?= base_url().'/assets/vendor/aos/aos.js'?>"></script>
-  <script src="<?= base_url().'/assets/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
-  <script src="<?= base_url().'/assets/vendor/glightbox/js/glightbox.min.js'?>"></script>
-  <script src="<?= base_url().'/assets/vendor/swiper/swiper-bundle.min.js'?>"></script>
-  <script src="<?= base_url().'/assets/vendor/php-email-form/validate.js'?>"></script>
+  <!-- endor JS Files -->
+  <script src="<?= base_url().'/assets/endor/purecounter/purecounter.js'?>"></script>
+  <script src="<?= base_url().'/assets/endor/aos/aos.js'?>"></script>
+  <script src="<?= base_url().'/assets/endor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+  <script src="<?= base_url().'/assets/endor/glightbox/js/glightbox.min.js'?>"></script>
+  <script src="<?= base_url().'/assets/endor/swiper/swiper-bundle.min.js'?>"></script>
+  <script src="<?= base_url().'/assets/endor/php-email-form/validate.js'?>"></script>
 
   <!-- Template Main JS File -->
   <script src="<?= base_url().'/assets/js/main.js'?>"></script>
