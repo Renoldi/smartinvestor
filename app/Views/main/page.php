2ac96be79db2faa0203b8c3e1d038eea40992112
renoldiboz <renoldiboz@gmail.com>
2022-02-18 21:17:57 +0700

    change

--- app/Views/main/page.php
+++ app/Views/main/page.php
@@ -10,43 +10,49 @@ foreach ($pages as $key => $page) {
     foreach ($page as $section) {
         if ($section->display == "all") {
             if ($x % 2) {
-                $container .= '
-            <div class="col-md-4" data-aos="fade-right">
-                <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-8 pt-4" data-aos="fade-up">
-            ' .  $section->decs . '
-            </div><div class="col-md-12 " ></div>';
+                $container .= 
+                '<div class="row content" >
+                    <div class="col-md-4" data-aos="fade-right">
+                        <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8 pt-4" data-aos="fade-up">
+                    ' .  $section->decs . '
+                    </div> 
+                </div>';
             } else {
                 $container .=
-                    '<div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
-                <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-            ' .  $section->decs . '
-            </div><div class="col-md-12 " ></div>
+                '<div class="row content" ><div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+                        <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
+                </div>
+                    <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
+                    ' .  $section->decs . '
+                    </div> 
+                </div>
         ';
             }
         } else if ($section->display == "img") {
             $container .=
-                '<div class="col-md-12" data-aos="fade-left">
-            <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
-        </div><div class="col-md-12 " ></div>';
+            '<div class="row content" >
+                <div class="col-md-12" data-aos="fade-left">
+                    <img src="' . base_url("assets/img/" . $section->image) . '" class="img-fluid" alt="">
+                </div> 
+            </div>';
         } else {
             $container =
-                ' <div class="col-md-12 " data-aos="fade-up">
-            ' .  $section->decs . '
-            </div><div class="col-md-12 " ></div>';
+                '<div class="row content" > 
+                    <div class="col-md-12 " data-aos="fade-up">
+                    ' .  $section->decs . '
+                    </div>
+                </div>';
         }
         $x++;
     }
     $show .=
         '<section id="' . $key . '" class="details">
             <div class="container">
-                <div class="row content">
+
                     ' . $container . '
-                </div>
-            </div>
+                            </div>
         </section >';
 }
 
