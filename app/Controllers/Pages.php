33a0515a4d73d20d59df83fe34b4c208aef439fb
renoldiboz <renoldiboz@gmail.com>
2022-02-20 21:42:20 +0700

    change

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('pages');
+    }
+
+    public function uploadImages()
+    {
+        // $validated = $this->validate([
+        //     'upload' => [
+        //         'uploaded[upload]',
+        //         'mime_in[upload,image/jpg,image/jpeg,image/png]',
+        //         'max_size[upload,1024]',
+        //     ],
+        // ]);
+
+        // $file = $this->request->getFile('upload');
+        // // if ($validated) {
+        // //     $fileName = $file->getRandomName();
+        // //     $writePath = './upload/';
+        // //     $file->move($writePath, $fileName);
+        // //     $data = [
+        // //         "uploaded" => true,
+        // //         "url" => base_url($writePath . $fileName),
+        // //     ];
+        // // } else {
+        //     $data = [
+        //         "uploaded" => false,
+        //         "error" => [
+        //             "messsages" => $file
+        //         ],
+        //     ];
+        // // }
+        // return $this->response->setJSON($data);
+        var_dump($this->input->post());
+        /*--OR---*/
+        var_dump($_REQUEST);
+         /*--OR---*/
+        print_r($this->input->post());
+        die;
+    }
+}
---
+++ app/Views/pages.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <form class="row g-3 needs-validation" novalidate>
+          <div class="col-12">
+            <label for="exampleFormControlInput1" class="form-label">Menu</label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <label for="exampleFormControlInput1" class="form-label">Section</label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected value="about">about</option>
+              <option value="profit">profit</option>
+              <option value="package">package</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <label for="exampleFormControlInput1" class="form-label">Section</label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected value="all">all</option>
+              <option value="img">img</option>
+              <option value="desc">desc</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <label for="exampleFormControlTextarea1" class="form-label">descrition</label>
+            <div class="row">
+              <div class="document-editor__toolbar"></div>
+            </div>
+            <textarea class="form-control editor" id="exampleFormControlTextarea1" rows="0"></textarea>
+          </div>
+          <div class="col-12">
+            <label for="formFile" class="form-label">Default file input example</label>
+            <input class="form-control" type="file" id="formFile">
+          </div>
+          <div class="col-12">
+            <button class="btn btn-primary" type="submit">Submit form</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="<?= base_url('ckeditor5/build/ckeditor.js') ?>"></script>
+  <script>
+    ClassicEditor
+      .create(document.querySelector('.editor'), {
+        ckfinder: {
+          uploadUrl: "<?= base_url('pages/uploadImages') ?>",
+        },
+      })
+      .then(editor => {
+        window.editor = editor;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
